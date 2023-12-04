66b984ed6bb138e80b2e2dff73978f23519470ad
radrien <raphael.adrien@daimler.com>
2023-12-04 19:08:21 +0000

    Adiciona dependencias do JPA

--- primefaces/src/main/java/br/jus/tjms/primefaces/entidade/Servidor.java
+++ primefaces/src/main/java/br/jus/tjms/primefaces/entidade/Servidor.java
@@ -1,12 +1,24 @@
 package br.jus.tjms.primefaces.entidade;
 
+import javax.persistence.Entity;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.math.BigDecimal;
 import java.util.Date;
+import java.util.UUID;
 
+@Entity
+@Table(name = "servidores")
 public class Servidor {
 
+    @Id
+    @GeneratedValue
+    private UUID id;
     private String cpf;
     private String nome;
+    @Enumerated
     private TipoServidor tipoServidor;
     private BigDecimal salario;
     private Date dataAprovacao;
