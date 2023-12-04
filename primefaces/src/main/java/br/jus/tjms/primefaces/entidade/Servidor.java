550dcffa3f25bed4a3f9692b88670cca8516e66f
radrien <raphael.adrien@daimler.com>
2023-12-04 08:36:19 +0000

    Adiciona validacao

--- primefaces/src/main/java/br/jus/tjms/primefaces/entidade/Servidor.java
+++ primefaces/src/main/java/br/jus/tjms/primefaces/entidade/Servidor.java
@@ -1,12 +1,15 @@
 package br.jus.tjms.primefaces.entidade;
 
+import java.math.BigDecimal;
+import java.util.Date;
+
 public class Servidor {
 
     private String cpf;
-
     private String nome;
-
     private TipoServidor tipoServidor;
+    private BigDecimal salario;
+    private Date dataAprovacao;
 
     public String getCpf() {
         return cpf;
@@ -30,5 +33,32 @@ public class Servidor {
 
     public void setTipoServidor(TipoServidor tipoServidor) {
         this.tipoServidor = tipoServidor;
+    }
+
+    public BigDecimal getSalario() {
+        return salario;
+    }
+
+    public void setSalario(BigDecimal salario) {
+        this.salario = salario;
+    }
+
+    public Date getDataAprovacao() {
+        return dataAprovacao;
+    }
+
+    public void setDataAprovacao(Date dataAprovacao) {
+        this.dataAprovacao = dataAprovacao;
+    }
+
+    @Override
+    public String toString() {
+        return "Servidor{" +
+            "cpf='" + cpf + '\'' +
+            ", nome='" + nome + '\'' +
+            ", tipoServidor=" + tipoServidor +
+            ", salario=" + salario +
+            ", dataAprovacao=" + dataAprovacao +
+            '}';
     }
 }
